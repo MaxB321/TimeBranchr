24b997c1fe7dcd2582bed92f2bb04f58e661f974
DaoOfBullshit <max.bender31@gmail.com>
2026-02-02 19:54:20 -0500

    log tree widget gets refreshes display when new logs added

--- src/gui/widgets/log_widget.py
+++ src/gui/widgets/log_widget.py
@@ -9,35 +9,25 @@ class LogWidget():
         self._user_logs: dict[str, list[int]] = {}
         self._category_id: str = ""
 
+
     def add_log(self, parent: QTreeWidget, seconds: int) -> None:  # add timer log after stop btn pressed
-        s = seconds % 60
-        minutes = (seconds % 3600) // 60
-        hours = seconds // 3600
-        log_str = f"{hours:02}:{minutes:02}:{s:02}"
-
-        if not (seconds == 0):
-            new_log = QTreeWidgetItem(parent)
-            new_log.setText(0, log_str)
-            self._user_logs[self._category_id].append(seconds)
-
+        if seconds == 0:
+            return
+        
+        self._user_logs[self._category_id].append(seconds)
+        self.display_logs_nto(parent)
 
 
     def connect_log(self, category_tree: QTreeWidget, log_tree: QTreeWidget) -> None:  # connect log to category
         cur_item = category_tree.currentItem()
         if not cur_item:
             return
-        log_tree.clear()
+
         cur_item_name = cur_item.text(0)
         header = log_tree.headerItem()
         header.setText(0, f"Logs - {cur_item_name}")
 
-        for val in reversed(self._user_logs[self._category_id]):
-            s = val % 60
-            m = (val % 3600) // 60
-            h = val // 3600
-            log_str = f"{h:02}:{m:02}:{s:02}" 
-            new_log = QTreeWidgetItem(log_tree)
-            new_log.setText(0, log_str)
+        self.display_logs_nto(log_tree)
         
 
     def create_log(self):  # user-created log 
@@ -48,9 +38,21 @@ class LogWidget():
         pass
 
 
-    def set_category_id(self, category_id: str):
-        self._category_id = category_id
+    def display_logs_nto(self, parent: QTreeWidget):
+        parent.clear()
+        for val in reversed(self._user_logs[self._category_id]):
+            s = val % 60
+            m = (val % 3600) // 60
+            h = val // 3600
+            log_str = f"{h:02}:{m:02}:{s:02}" 
+            new_log = QTreeWidgetItem(parent)
+            new_log.setText(0, log_str)
 
 
     def init_category(self, category_id: str):
         self._user_logs[category_id] = []
+
+
+    def set_category_id(self, category_id: str):
+        self._category_id = category_id
+    
