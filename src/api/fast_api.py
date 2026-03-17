007467f6e3a4c3dddbc7787f553bc2a8da333b30
DaoOfBullshit <max.bender31@gmail.com>
2026-03-17 15:27:43 -0400

    added rate limiting to api

--- src/api/fast_api.py
+++ src/api/fast_api.py
@@ -1,8 +1,22 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
 from database.db_connect import engine
+from slowapi import Limiter
+from slowapi.errors import RateLimitExceeded
+from slowapi.middleware import SlowAPIMiddleware
+from slowapi import _rate_limit_exceeded_handler
 
 
 app_api = FastAPI()
+
+
+def get_real_ip(request: Request) -> str:
+    return request.headers.get("X-Forwarded-For", request.client.host)
+
+limiter = Limiter(key_func=get_real_ip, default_limits=["100/minute"])
+
+app_api.state.limiter = limiter
+app_api.add_exception_handler(RateLimitExceeded, _rate_limit_exceeded_handler)
+app_api.add_middleware(SlowAPIMiddleware)
 
 
 from api.routes import api_categories_table, api_logs_table, api_user_table
